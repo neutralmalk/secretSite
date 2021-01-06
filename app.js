34c392da06e8a46ffab3d4cae8d9e7750fa20cc7
Jonathan Tyrrell <jon.mtyrrell@gmail.com>
2021-01-06 11:58:47 -0500

    hashing practice

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -35,7 +35,7 @@ app.route("/login")
   })
   .post(function(req,res){
     const userName = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: userName}, function(err, foundUser){
       if(err){
@@ -59,7 +59,7 @@ app.route("/register")
   .post(function(req, res){
     const newUser = new User({
       email: req.body.username,
-      password : req.body.password
+      password : md5(req.body.password)
     });
     newUser.save(function(err){
       if(err){
